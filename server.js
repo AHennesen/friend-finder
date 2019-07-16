70f084644e33b9fd4392aa0e78f74f0bd8a44ab3
Andrew H <ahennesen@gmail.com>
2019-07-16 16:03:39 -0400

    added the routes for the app to work

--- server.js
+++ server.js
@@ -2,19 +2,21 @@ var express = require("express");
 var path = require("path");
 var bodyParser = require('body-parser')
 
-
+// tells node that we are created an express server
 var app = express();
 
-
+// sets a base port and a heroku development port
 var PORT = process.env.PORT || 8080;
+
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-
+// shows the app where to when when certain urls are visited
 require("./app/routing/apiRoutes")(app);
 require("./app/routing/htmlRoutes")(app);
 
+// starts the app, telles it what port to listen too
 app.listen(PORT, function() {
     console.log("App listening on PORT: " + PORT);
 });
