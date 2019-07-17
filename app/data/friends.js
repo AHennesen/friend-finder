a4f43f6d225284bd50922fd2ef4996a509d26671
Andrew H <ahennesen@gmail.com>
2019-07-16 21:54:10 -0400

    finshed adding tocuhes to data friends.js

---
+++ app/data/friends.js
@@ -0,0 +1,66 @@
+var friends = [
+    {
+        name: "Pierce",
+        photo: "https://upload.wikimedia.org/wikipedia/en/c/c2/Peter_Griffin.png",
+        scores: [
+            "1",
+            "2",
+            "3",
+            "4",
+            "5",
+            "1",
+            "2",
+            "3",
+            "4",
+            "5",
+        ]
+    },
+    {
+        name: "David",
+        photo: "https://the-big-bang-theory.com/images/uploads/5/rs_3463ec66edb459bd1dd.jpg",
+        scores: [
+            "2",
+            "1",
+            "3",
+            "4",
+            "5",
+            "2",
+            "1",
+            "4",
+            "2",
+            "1",
+        ]
+    },
+    {
+        name: "Elvin",
+        photo: "https://upload.wikimedia.org/wikipedia/commons/7/7f/Michio_Kaku_Presentation.jpg",
+        scores: [
+            "1",
+            "1",
+            "1",
+            "1",
+            "1",
+            "1",
+            "1",
+            "1",
+            "1",
+            "1",
+        ]
+    },
+    {
+        name: "Josh",
+        photo: "https://img.thedailybeast.com/image/upload/c_crop,d_placeholder_euli9k/dpr_1.5/c_limit,w_690/fl_lossy,q_auto/v1/galleries/2014/03/18/ugliest-celebrity-beards-angus-t-jones-brad-pitt-more-photos/worst-celeb-beards-joaquin_fkmngr",
+        scores: [
+            "5",
+            "5",
+            "5",
+            "5",
+            "5",
+            "5",
+            "5",
+            "5",
+            "5",
+            "5",
+        ]
+    }
+]
